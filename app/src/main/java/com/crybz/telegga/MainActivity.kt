52bfa8abbefbc06c62ecf4f1f3692558555eed97
Igor-Leontyev <79319381+Igor-Leontyev@users.noreply.github.com>
2022-03-13 01:48:16 +0300

    Refactoring

--- app/src/main/java/com/crybz/telegga/MainActivity.kt
+++ app/src/main/java/com/crybz/telegga/MainActivity.kt
@@ -8,6 +8,7 @@ import android.widget.Toolbar
 import com.crybz.telegga.databinding.ActivityMainBinding
 import com.crybz.telegga.ui.ChatFragment
 import com.crybz.telegga.ui.SettingsFragment
+import com.crybz.telegga.ui.objects.AppDrawer
 import com.mikepenz.materialdrawer.AccountHeader
 import com.mikepenz.materialdrawer.AccountHeaderBuilder
 import com.mikepenz.materialdrawer.Drawer
@@ -21,8 +22,8 @@ import com.mikepenz.materialdrawer.model.interfaces.IDrawerItem
 class MainActivity : AppCompatActivity() {
 
     private lateinit var mBinding: ActivityMainBinding
-    private lateinit var mDrawer: Drawer
-    private lateinit var mHeder: AccountHeader
+    private lateinit var mAppDrawer: AppDrawer
+
     private lateinit var mToolBar: androidx.appcompat.widget.Toolbar
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -35,93 +36,21 @@ class MainActivity : AppCompatActivity() {
         super.onStart()
         initFields()
         initFunc()
+        mAppDrawer.create()
     }
 
     private fun initFunc() {
         setSupportActionBar(mToolBar)
+
         supportFragmentManager.beginTransaction()
             .replace(R.id.dataContainer,ChatFragment()).commit()
-        createheader()
-        createDrawer()
-    }
-
-    private fun createDrawer() {
-        mDrawer = DrawerBuilder().withActivity(this)
-            .withToolbar(mToolBar).withActionBarDrawerToggle(true)
-            .withSelectedItem(-1)
-            .withAccountHeader(mHeder).addDrawerItems(
-                PrimaryDrawerItem().withIdentifier(101)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать группу")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_create_groups),
-                PrimaryDrawerItem().withIdentifier(102)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать секретный чат")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_secret_chat),
-                PrimaryDrawerItem().withIdentifier(103)
-                    .withIconTintingEnabled(true)
-                    .withName("Создать канал")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_create_channel),
-                PrimaryDrawerItem().withIdentifier(104)
-                    .withIconTintingEnabled(true)
-                    .withName("Контакты")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_contacts),
-                PrimaryDrawerItem().withIdentifier(105)
-                    .withIconTintingEnabled(true)
-                    .withName("Звонки")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_phone),
-                PrimaryDrawerItem().withIdentifier(106)
-                    .withIconTintingEnabled(true)
-                    .withName("Избранное")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_favorites),
-                PrimaryDrawerItem().withIdentifier(107)
-                    .withIconTintingEnabled(true)
-                    .withName("Настройки")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_settings),
-                DividerDrawerItem(),
-                PrimaryDrawerItem().withIdentifier(108)
-                    .withIconTintingEnabled(true)
-                    .withName("Пригласить друзей")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_invate),
-                PrimaryDrawerItem().withIdentifier(109)
-                    .withIconTintingEnabled(true)
-                    .withName("Вопросы")
-                    .withSelectable(false)
-                    .withIcon(R.drawable.ic_menu_help)
-
-
-            ).withOnDrawerItemClickListener(object :Drawer.OnDrawerItemClickListener{
-                override fun onItemClick(view: View?, position: Int, drawerItem: IDrawerItem<*>): Boolean {
-                 when(position){
-                     7 ->   supportFragmentManager.beginTransaction()
-                         .addToBackStack(null)
-                         .replace(R.id.dataContainer,SettingsFragment()).commit()
-
-                 }
-                   return  false
-                }
-            }).build()
 
     }
 
-    private fun createheader() {
-        mHeder = AccountHeaderBuilder().withActivity(this)
-            .withHeaderBackground(R.drawable.heder).addProfiles(
-                ProfileDrawerItem().withName("sergey Zorin")
-                    .withEmail("+895537231")
-            ).build()
 
-    }
 
     private fun initFields() {
         mToolBar = mBinding.mainToolBar
+        mAppDrawer = AppDrawer(this,mToolBar)
     }
 }
